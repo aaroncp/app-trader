c9b86962f98d7337f8556673efb3d179ad08325c
Philton-maker <campbell.phil8@gmail.com>
2020-08-22 11:59:00 -0500

    finished genre analysis and added visualizations

--- script/script_for_project.sql
+++ script/script_for_project.sql
@@ -1,19 +1,23 @@
-/*select p.genres, p.review_count
-FROM play_store_apps p
-INNER JOIN app_store_apps a
-ON p.name = a.name;*/
+/*SELECT DISTINCT genre_clean, ROUND(AVG(avg_rating),2) AS total_avg_rating
+FROM
+	(SELECT DISTINCT REPLACE(REPLACE(TRIM(UPPER(primary_genre)),' ','_'),'&','AND') AS genre_clean, ROUND(AVG(rating),2) AS avg_rating		FROM app_store_apps
+	WHERE price <= '1.00' AND rating IS NOT NULL
+	GROUP BY primary_genre
+	UNION
+	SELECT DISTINCT category, ROUND(AVG(rating),2)
+	FROM play_store_apps
+	WHERE price <= '1.00' AND rating IS NOT NULL
+	GROUP BY category) AS subq
+GROUP BY genre_clean
+ORDER BY ROUND(AVG(avg_rating),2) DESC
+LIMIT 10;*/
 
---above query is for testing
+--The above query brings together the primary_genre and category columns and gives their averages across stores.
 
+SELECT DISTINCT genres, ROUND(AVG(rating),2)
+FROM play_store_apps
+WHERE price <= '1.00' AND rating IS NOT NULL
+GROUP BY genres
+ORDER BY ROUND(AVG(rating),2) DESC;
 
-/*SELECT p.genres, ROUND(((AVG(p.rating) + AVG(a.rating)) / 2), 2) AS avg_rating
-FROM play_store_apps p
-INNER JOIN app_store_apps a
-ON p.name = a.name
-GROUP BY p.genres, p.category
-ORDER BY avg_rating DESC
-LIMIT 10;
-
-We can see from this that the highest rated genres of apps across both stores are interactive education apps, books and references apps, brain/board game apps
-(think words with friends), strategy and sports games, and family simulation apps.
-All of this to say: people love interactive experience apps that make them feel like they're learning or improving.*/
+--The above query shows the genres with the top 10 average ratings from the google play store.
